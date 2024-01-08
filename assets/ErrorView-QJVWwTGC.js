b09e805af01d235c724a6302509c75901aedde54
BelgianNoise <38424924+BelgianNoise@users.noreply.github.com>
2024-01-08 01:00:25 +0000

    Deploying to gh-pages from @ BelgianNoise/bingo-card-generator@53310735fb5a4dcc561869adf58f04ac3d5babeb 🚀

---
+++ assets/ErrorView-QJVWwTGC.js
@@ -0,0 +1,1 @@
+import{d as t,m as a,o as s,c as n,a as e,C as c,P as l,R as u}from"./index-vMAS2FgS.js";const _=e("h1",null,"Error",-1),f=t({__name:"ErrorView",setup(m){const o=u(),r=a(o.query.code);return(p,d)=>(s(),n(l,null,[_,e("h2",null,c(r.value),1)],64))}});export{f as default};
