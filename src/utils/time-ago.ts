a24bc79052a31cfefffb847ec20afdbdd264e6f3
Arthur Joppart <arthur@digita.ai>
2024-01-08 19:34:16 +0100

    improve time ago a lil

--- src/utils/time-ago.ts
+++ src/utils/time-ago.ts
@@ -2,22 +2,37 @@ export function timeAgo(date: number) {
   const seconds = Math.floor((new Date().getTime() - date) / 1000);
   let interval = Math.floor(seconds / 31536000);
   if (interval >= 1) {
+    if (interval === 1) {
+      return interval + " year ago";
+    }
     return interval + " years ago";
   }
   interval = Math.floor(seconds / 2592000);
   if (interval >= 1) {
+    if (interval === 1) {
+      return interval + " month ago";
+    }
     return interval + " months ago";
   }
   interval = Math.floor(seconds / 86400);
   if (interval >= 1) {
+    if (interval === 1) {
+      return interval + " day ago";
+    }
     return interval + " days ago";
   }
   interval = Math.floor(seconds / 3600);
   if (interval >= 1) {
+    if (interval === 1) {
+      return interval + " hour ago";
+    }
     return interval + " hours ago";
   }
   interval = Math.floor(seconds / 60);
   if (interval >= 1) {
+    if (interval === 1) {
+      return interval + " minute ago";
+    }
     return interval + " minutes ago";
   }
   if (seconds > 5) {
